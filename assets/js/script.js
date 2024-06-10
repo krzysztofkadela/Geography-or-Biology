af533bd2027ea354183a6080fda9850902474c9c
Krzysztof Kadela <krzysztofkadela01@gmail.com>
2024-06-10 19:44:08 +0000

    Feat: Add basic structure in js script.file.

---
+++ assets/js/script.js
@@ -0,0 +1,37 @@
+//set questions and 4 answers for each, only one correct.
+
+
+
+
+
+
+// Wait for DOM to finish loading , get buttons and add events listeners to them
+
+
+// Start quiz function , takes a parameter to select the appropriate quiz.
+
+function startQuize(quizType){
+// if Geo
+loadGeoQuestions();
+// else
+loadBioQuestions();
+}
+
+
+
+// 2 functions to display(load) questions for diferent quiz type.
+
+function loadGeoQuestions(){
+
+}
+
+function loadBioQuestions(){
+
+}
+
+//Check ansver function, to check answer and increase conter for correct or inncorect.
+
+function checkAnswer(){
+
+}
+
