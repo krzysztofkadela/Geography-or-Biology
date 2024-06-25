1605363559bbdb4fc540214e541d2b93b065d9a4
Krzysztof Kadela <krzysztofkadela01@gmail.com>
2024-06-25 17:16:58 +0000

    Script: Add new function to set quiz questions in random order.

--- assets/js/script.js
+++ assets/js/script.js
@@ -173,18 +173,16 @@ function startQuize(quizType) {
 
 }
 
+//second option for random number
+function setQuestionsInRandomPositions() {
+    let numbers = [1, 2, 3, 4]; // Number of questions
+    let shuffledNumbers = numbers.sort(() => Math.random() - 0.5);
+    return shuffledNumbers;
+}
 
-// Function to shuffle an array
-function shuffleArray(array) {
-    for (let i = array.length - 1; i > 0; i--) {
-        const j = Math.floor(Math.random() * (i + 1));
-        [array[i], array[j]] = [array[j], array[i]];
-    }
-    return array;
-};
-
-const shuffledNumbers = shuffleArray(array);
-
+// Call the function to get an array with 4 numbers in random positions
+let result = setQuestionsInRandomPositions();
+console.log(result);
 
 function loadGeoQuestions() {
     deselectAnswers();
