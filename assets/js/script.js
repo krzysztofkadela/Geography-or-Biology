6ead405d8e38f6386de84f1ee0c8357065d940ec
Krzysztof Kadela <krzysztofkadela01@gmail.com>
2024-06-25 20:29:05 +0000

    Script: Update function changeBackgroundImage.

--- assets/js/script.js
+++ assets/js/script.js
@@ -258,5 +258,5 @@ function cleareInput() {
 function changeBackgroundImage(id){
 
  let elementId = document.getElementById(id);
- elementId.style.backgroundColor='green';
+ elementId.style.background='url(assets/images/pexels-sadface.jpg) no-repeat center center/cover';
 }
